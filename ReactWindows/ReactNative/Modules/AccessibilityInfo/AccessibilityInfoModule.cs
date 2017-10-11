a6634d3c3d169216d421801c61e03bdbee425283
Anton Khayrudinov <antonkh@microsoft.com>
2017-10-10 19:03:17 -0700

    Get the 8 high contrast colors

--- ReactWindows/ReactNative/Modules/AccessibilityInfo/AccessibilityInfoModule.cs
+++ ReactWindows/ReactNative/Modules/AccessibilityInfo/AccessibilityInfoModule.cs
@@ -8,6 +8,28 @@ namespace ReactNative.Modules.Accessibilityinfo
     class AccessibilityInfoModule : ReactContextNativeModuleBase
     {
         private readonly AccessibilitySettings _accessibility = new AccessibilitySettings();
+        private readonly UISettings _settings = new UISettings();
+
+        private string GetHexString(UIElementType type)
+        {
+            var color = _settings.UIElementColor(type);
+            return "rgba(" + color.R + "," + color.G + "," + color.B + "," + color.A + ")";
+        }
+
+        private IDictionary<string, string> GetHighContrastColors()
+        {
+            return new Dictionary<string, string>
+            {
+                {  "windowText", GetHexString(UIElementType.WindowText) },
+                {  "hotlight", GetHexString(UIElementType.Hotlight) },
+                {  "grayText", GetHexString(UIElementType.GrayText) },
+                {  "highlightText", GetHexString(UIElementType.HighlightText) },
+                {  "highlight", GetHexString(UIElementType.Highlight) },
+                {  "buttonText", GetHexString(UIElementType.ButtonText) },
+                {  "buttonFace", GetHexString(UIElementType.ButtonFace) },
+                {  "window", GetHexString(UIElementType.Window) },
+            };
+        }
 
         public AccessibilityInfoModule(ReactContext reactContext)
             : base(reactContext)
